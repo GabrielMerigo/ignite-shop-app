3266eea749419d694d5672b04cfe28ff12c569e0
Gabriel <gabriel.merigo.dev@gmail.com>
2024-01-22 16:38:21 -0300

    creating foreground feature

--- App.tsx
+++ App.tsx
@@ -1,8 +1,7 @@
 import { StatusBar } from "react-native";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { NativeBaseProvider } from "native-base";
-import { OSNotification, OneSignal } from "react-native-onesignal";
-import { NotificationEventTypeMap } from "react-native-onesignal/dist/models/NotificationEvents";
+
 import {
   useFonts,
   Roboto_400Regular,
@@ -16,24 +15,12 @@ import { Loading } from "./src/components/Loading";
 
 import { CartContextProvider } from "./src/contexts/CartContext";
 import { oneSignalInitialize } from "./src/libs/onesignal";
-import { Notification } from "./src/components/Notification";
 
 export default function App() {
-  const [notification, setNotification] = useState<OSNotification | null>();
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   useEffect(() => {
     oneSignalInitialize()
-  }, [])
-
-  useEffect(() => {
-    const unsubscribe = OneSignal.Notifications.addEventListener('foregroundWillDisplay', (notificationReceivedEvent: NotificationEventTypeMap['foregroundWillDisplay']) => {
-      const response = notificationReceivedEvent.getNotification();
-
-      setNotification(response);
-    })
-
-    return () => unsubscribe
   }, [])
 
   return (
@@ -46,8 +33,6 @@ export default function App() {
       <CartContextProvider>
         {fontsLoaded ? <Routes /> : <Loading />}
       </CartContextProvider>
-
-      {notification?.title && <Notification title={notification.title} onClose={() => setNotification(null)} />}
     </NativeBaseProvider>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,36 @@
+import { useEffect, useState } from 'react';
+import { OSNotification } from 'react-native-onesignal';
 import { useTheme } from 'native-base';
+import { OneSignal } from 'react-native-onesignal';
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
+
+import { Notification } from '../components/Notification';
+import { NotificationEventTypeMap } from 'react-native-onesignal/dist/models/NotificationEvents';
 
 import { AppRoutes } from './app.routes';
 
 export function Routes() {
+  const [notification, setNotification] = useState<OSNotification | null>();
   const { colors } = useTheme();
 
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
 
+  useEffect(() => {
+    const unsubscribe = OneSignal.Notifications.addEventListener('foregroundWillDisplay', (notificationReceivedEvent: NotificationEventTypeMap['foregroundWillDisplay']) => {
+      const response = notificationReceivedEvent.getNotification();
+
+      setNotification(response);
+    })
+
+    return () => unsubscribe
+  }, [])
+
   return (
     <NavigationContainer theme={theme}>
       <AppRoutes />
+
+      {notification?.title && <Notification data={notification} onClose={() => setNotification(null)} />}
     </NavigationContainer>
   );
 }
