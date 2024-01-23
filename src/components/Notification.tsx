852af35094fe1fcf9c97ef608b579a96876db842
Gabriel <gabriel.merigo.dev@gmail.com>
2024-01-23 09:16:07 -0300

    adding expo-linking

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,26 +1,20 @@
 import { HStack, Text, IconButton, CloseIcon, Icon, Pressable } from 'native-base';
 import { Ionicons } from '@expo/vector-icons';
 import { OSNotification } from 'react-native-onesignal';
-import { useNavigation } from '@react-navigation/native';
+import * as Linking from 'expo-linking';
+
 
 type Props = {
   data: OSNotification;
   onClose: () => void;
 }
 
-type AdditionalDataProps = {
-  route?: 'details';
-  product_id?: string;
-};
-
 export function Notification({ data, onClose }: Props) {
-  const { navigate } = useNavigation();
+  console.log(data)
 
   function handleOnPress() {
-    const { route, product_id } = data.additionalData as AdditionalDataProps;
-
-    if(route && product_id){ 
-      navigate(route, { productId: product_id })
+    if(data.launchURL) {
+      Linking.openURL(data.launchURL);
       onClose();
     }
   }
